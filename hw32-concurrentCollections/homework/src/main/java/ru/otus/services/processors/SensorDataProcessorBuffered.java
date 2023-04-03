57345fa26b918f8c72a7aaed454db8af87e1cc3e
Alexander <khorevalexandr1@gmail.com>
2023-04-03 21:36:41 +0300

    homework done

--- hw32-concurrentCollections/homework/src/main/java/ru/otus/services/processors/SensorDataProcessorBuffered.java
+++ hw32-concurrentCollections/homework/src/main/java/ru/otus/services/processors/SensorDataProcessorBuffered.java
@@ -6,30 +6,45 @@ import ru.otus.lib.SensorDataBufferedWriter;
 import ru.otus.api.SensorDataProcessor;
 import ru.otus.api.model.SensorData;
 
-// Этот класс нужно реализовать
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.PriorityBlockingQueue;
+
 public class SensorDataProcessorBuffered implements SensorDataProcessor {
     private static final Logger log = LoggerFactory.getLogger(SensorDataProcessorBuffered.class);
 
     private final int bufferSize;
     private final SensorDataBufferedWriter writer;
+    private final BlockingQueue<SensorData> bufferData;
 
     public SensorDataProcessorBuffered(int bufferSize, SensorDataBufferedWriter writer) {
         this.bufferSize = bufferSize;
         this.writer = writer;
+        // Используется PriorityBlockingQueue, чтобы извлекать объекты в порядке возрастания времени измерения
+        this.bufferData = new PriorityBlockingQueue<>(bufferSize, Comparator.comparing(SensorData::getMeasurementTime));
     }
 
     @Override
     public void process(SensorData data) {
-    /*
-        if (dataBuffer.size() >= bufferSize) {
+        if (bufferData.size() >= bufferSize) {
+            log.info("Буфер заполнен -> запись и очистка буфера");
             flush();
         }
-    */
+        if (bufferData.offer(data)) {
+            log.info("Данные добавлены в буфер: {}", data);
+        }
     }
 
     public void flush() {
         try {
-            //writer.writeBufferedData(bufferedData);
+            if (bufferData.size() == 0) {
+                log.info("Буфер пуст");
+            } else {
+                var bufferToWrite = new ArrayList<SensorData>();
+                bufferData.drainTo(bufferToWrite);
+                writer.writeBufferedData(bufferToWrite);
+            }
         } catch (Exception e) {
             log.error("Ошибка в процессе записи буфера", e);
         }

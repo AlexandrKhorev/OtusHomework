9662d9052a25891ac617e7afa439603131e2daf8
Alexander <khorevalexandr1@gmail.com>
2023-04-04 17:32:11 +0300

    init homework

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -2,4 +2,8 @@ public interface Versions {
     String guava = "31.1-jre";
     String asm = "9.4";
     String lombok = "1.18.24";
+    String sockjs = "1.5.1";
+    String stomp = "2.3.4";
+    String bootstrap = "5.2.3";
+    String r2dbcPostgresql = "0.8.13.RELEASE";
 }

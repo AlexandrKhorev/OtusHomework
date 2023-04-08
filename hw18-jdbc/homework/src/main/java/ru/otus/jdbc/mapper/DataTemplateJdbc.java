246e92a487c526d3bcb6ea28bbf09309be1418bc
Alexander <khorevalexandr1@gmail.com>
2023-04-08 13:09:36 +0300

    init homework

---
+++ hw18-jdbc/homework/src/main/java/ru/otus/jdbc/mapper/DataTemplateJdbc.java
@@ -0,0 +1,42 @@
+package ru.otus.jdbc.mapper;
+
+import ru.otus.core.repository.DataTemplate;
+import ru.otus.core.repository.executor.DBExecutor;
+
+import java.sql.Connection;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Сохратяет объект в базу, читает объект из базы
+ */
+public class DataTemplateJdbc<T> implements DataTemplate<T> {
+
+    private final DBExecutor dbExecutor;
+    private final EntitySQLMetaData entitySQLMetaData;
+
+    public DataTemplateJdbc(DBExecutor dbExecutor, EntitySQLMetaData entitySQLMetaData) {
+        this.dbExecutor = dbExecutor;
+        this.entitySQLMetaData = entitySQLMetaData;
+    }
+
+    @Override
+    public Optional<T> findById(Connection connection, long id) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<T> findAll(Connection connection) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public long insert(Connection connection, T client) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void update(Connection connection, T client) {
+        throw new UnsupportedOperationException();
+    }
+}
